c2bb377bd268adf84cc79e8a3f26c05fb066634c
Демид Хабаров <piratnabortu@gmail.com>
2025-02-14 04:58:46 +0300

    full-ready

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,35 +1,50 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { useEffect, useState } from "react"
+import { Link } from "react-router";
+import { useNavigate } from "react-router-dom";
+import logo from './assets/logo.png'
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+  const navigate = useNavigate();
+  const [partners, setPartners] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await window.api.getPartners()
+      console.log(res)
+      setPartners(res)
+    })()
+  }, [])
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
+      <div className="page-heading">
+        <img className="page-logo" src={logo} alt="" />
+        <h1>Партнеры</h1>
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
+      <ul className="partners-list">
+        {partners.map((partner) => {
+          return <li className="partner-card" key={partner.id} onClick={() => { navigate('/update', { state: { partner } }) }}>
+            <div className="partner-data">
+              <p className="card_heading">{partner.organization_type} | {partner.name}</p>
+              <div className="partner-data-info">
+                <p>{partner.ceo}</p>
+                <p>{partner.phone}</p>
+                <p>Рейтинг: {partner.rating}</p>
+              </div>
+            </div>
+            <div className="partner-sale partner-data card_heading">
+              {partner.discount}%
+            </div>
+          </li>
+        })}
+      </ul>
+
+      <Link to={'/create'}>
+        <button>
+          Создать партнера
+        </button>
+      </Link>
     </>
   )
 }
 
 export default App
-
--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,11 +1,19 @@
-import './assets/main.css'
+import './style.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Routes, Route, HashRouter } from 'react-router'
+import App from './App.jsx'
+import UpdatePartner from './UpdatePartner.jsx'
+import MakePartner from './MakePartner.jsx'
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <StrictMode>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/update' element={<UpdatePartner/>}/>
+        <Route path='/create' element={<MakePartner/>}/>
+      </Routes>
+    </StrictMode>
+  </HashRouter>
 )
